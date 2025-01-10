cafc8f80d59d66a10d6c7dc372c6c6c78483df8b
dieglitter <diegoguerreroalexander@gmail.com>
2025-01-10 11:51:53 -0600

    [setup]Clerk authentication done

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const  DashboardPage = () => {
   return (
-    <div>Landing Page (Protected)</div>
+    <div>
+      <p>Landing Page (Protected)</p>
+      <UserButton afterSwitchSessionUrl="/"/>
+
+    </div>
   );
 }
 
